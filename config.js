8546dc93eb017f9ff4505393668209b1b09df44e
Jason Sebring <mail@jasonsebring.com>
2015-06-05 08:31:48 -0700

    first push

---
+++ config.js
@@ -0,0 +1,17 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "traceur",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "jquery": "github:components/jquery@2.1.4",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
+  }
+});
+
