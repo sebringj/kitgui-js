3f4c2b69a7f6b0f531c01171098b73b16b549e0e
Jason Sebring <mail@jasonsebring.com>
2015-07-18 18:46:19 -0700

    es6 and jspm

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ System.config({
 
 System.config({
   "map": {
+    "handlebars": "github:components/handlebars.js@3.0.3",
     "jquery": "github:components/jquery@2.1.4",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
---
+++ test/kitgui.js
@@ -0,0 +1,1 @@
+../kitgui.js
